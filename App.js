6b4be061fb1f7ef982e5852a6007cf82ae5fec77
mac <mac@macdeMacBook-Pro.local>
2018-07-12 12:54:07 +0800

    完成商城，订单页面

--- App.js
+++ App.js
@@ -1,2 +1,6 @@
 import AppNavigator from './js/navigators/AppNavigator'
+// 关闭指定警告
+console.ignoredYellowBox = [ 'Warning: isMounted(...)','Warning: Failed prop type' ];
+// 关闭全部的警告
+//console.disableYellowBox = true;
 export default  AppNavigator;
